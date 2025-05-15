2393811182a77109e40c90b632148c7d1345801a
emmanuel <emmainebode80@@gmail.com>
2025-05-14 20:11:35 -0400

    week 2

---
+++ week2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>week2</title>
+</head>
+<body>
+
+    <?php
+    $fname = "Emmanuel";
+    $lname = "Inebode";
+
+    echo "<h1>Hello $fname $lname</h1>";
+
+    echo "<br><br>";
+    
+    echo "Hello " . $fname . " " . $lname;
+
+    echo "Hello, welcome to Emmanuel\"s world!";
+  ?>
+
+</body>
+</html>
